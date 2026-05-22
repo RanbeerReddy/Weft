08340348db86353ab347ef56629e7e58c38e8e00
RanbeerReddy <reddyranbeer@gmail.com>
2026-05-23 00:25:12 +0530

    doing a commmit for the sake of it

---
+++ Weft/core/reconstructing_chats.py
@@ -0,0 +1,6 @@
+import json
+from Weft.utils.exceptions import WeftException
+
+covo_path = "Data/Extracted data/"
+convo_files = ["conversations-000.json", "conversations-001.json", "conversations-002.json"]
+
